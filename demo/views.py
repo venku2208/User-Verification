edcc93472bf397a4f3335eea4bf9b63d4e0e0262
venku2208 <gvenku22@gmail.com>
2020-10-02 18:53:36 +0530

    user

---
+++ demo/views.py
@@ -0,0 +1,135 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignupForm
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User,auth
+from django.core.mail import EmailMessage
+from django.contrib import messages
+
+from datetime import datetime
+from django.core.exceptions import ObjectDoesNotExist
+import pyotp
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import phoneModel
+import base64
+
+
+def home(request):
+
+    return render(request,'base.html')
+
+def index(request):
+
+    return render(request,'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            email_subject = 'Activate your account.'
+            message = render_to_string('activation.html', 
+            {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(email_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        #return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+def signin(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        password= request.POST['password']
+
+        user =auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect("home")
+        else:
+            messages.info(request,'Invalid Credentials!')
+            return redirect('signin')
+    else:
+
+        return render(request,'signin.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
+class generateKey:
+    @staticmethod
+    def returnValue(phone):
+        return str(phone) + str(datetime.date(datetime.now())) + "Some Random Secret Key"
+
+
+class getPhoneNumberRegistered(APIView):
+    # Get to Create a call for OTP
+    @staticmethod
+    def get(request, phone):
+        try:
+            Mobile = phoneModel.objects.get(Mobile=phone)  # if Mobile already exists the take this else create New One
+        except ObjectDoesNotExist:
+            phoneModel.objects.create(
+                Mobile=phone,
+            )
+            Mobile = phoneModel.objects.get(Mobile=phone)  # user Newly created Model
+        Mobile.counter += 1  # Update Counter At every Call
+        Mobile.save()  # Save the data
+        keygen = generateKey()
+        key = base64.b32encode(keygen.returnValue(phone).encode())  # Key is generated
+        OTP = pyotp.HOTP(key)  # HOTP Model for OTP is created
+        print(OTP.at(Mobile.counter))
+        # Using Multi-Threading send the OTP Using Messaging Services like Twilio or Fast2sms
+        return Response({"OTP": OTP.at(Mobile.counter)}, status=200)  # Just for demonstration
+
+    # This Method verifies the OTP
+    @staticmethod
+    def post(request, phone):
+        try:
+            Mobile = phoneModel.objects.get(Mobile=phone)
+        except ObjectDoesNotExist:
+            return Response("User does not exist", status=404)  # False Call
+
+        keygen = generateKey()
+        key = base64.b32encode(keygen.returnValue(phone).encode())  # Generating Key
+        OTP = pyotp.HOTP(key)  # HOTP Model
+        if OTP.verify(request.data["otp"], Mobile.counter):  # Verifying the OTP
+            Mobile.isVerified = True
+            Mobile.save()
+            return Response("You are authorised", status=200)
+        return Response("OTP is wrong", status=400)
